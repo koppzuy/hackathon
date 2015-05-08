e1960e638c63b4987e9653ab283e18ec2bfd20e0
Daniel Tigges <danieltigges@googlemail.com>
2015-05-08 23:57:53 +0200

    wejrioper

---
+++ Padersprinter/Padersprinter/RealTimeStop.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Padersprinter.ViewModels;
+using PadersprinterService;
+using PadersprinterService.Parsing;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Padersprinter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RealTimeStop : Page
+    {
+        public RealTimeStop()
+        {
+            this.InitializeComponent();            
+            this.GetDummyStop();
+            this.DataContext = new RealTimeStopViewModel(7);
+        }
+
+        private async void GetDummyStop()
+        {
+            Stop s = (await Parser.FindStopsByNameAsync("Westerntor")).FirstOrDefault<Stop>();
+        }
+    }
+}
---
+++ Padersprinter/Padersprinter/ViewModels/RealTimeStopViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using PadersprinterService;
+using PadersprinterService.DataModel;
+using PadersprinterService.Parsing;
+
+namespace Padersprinter.ViewModels
+{
+    class RealTimeStopViewModel : BindableBase
+    {
+        private int stopShortName;
+        private Stop stop;
+        private ObservableCollection<ActualInfo> items = new ObservableCollection<ActualInfo>();
+        
+
+        public string Linie
+        {
+            get
+            {
+                return this.stop.ShortName.ToString();
+            }
+        }
+
+        public ObservableCollection<ActualInfo> Items
+        {
+            get
+            {
+                return items;
+            }
+        }
+
+        public RealTimeStopViewModel(int stopShortName)
+        {
+            Timer refreshTimer = new Timer(Refresh, null, 60000, 60000);
+            this.stopShortName = stopShortName;
+            this.Refresh(null);
+        }
+
+        private async void Refresh(object state)
+        {
+            if (this.stop != null)
+            {
+                this.stop = await Parser.GetStopFromShortNameAsync(this.stopShortName);
+            }
+            this.GetRealTimeInfo();
+        }
+
+        private async void GetRealTimeInfo()
+        {
+            this.Items.Clear();
+            RealTimeDepartureInfo info = await Parser.GetRealTimeDepartureInfoAsync(this.stop.ShortName, DirectionMode.Arrival);
+            foreach (ActualInfo item in info.Actual)
+            {
+                this.items.Add(item);
+            }
+
+        }
+    }
+}
